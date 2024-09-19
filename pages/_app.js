045abed8281097c955891d14f62a56131f313711
Dane <hyperiondane@gmail.com>
2024-09-19 16:35:55 +0200

    analytics code changes 2

--- pages/_app.js
+++ pages/_app.js
@@ -35,6 +35,12 @@ function MyApp({ Component, pageProps }) {
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}');`}
+      </Script>
       <Component {...pageProps} />
     </>
   );
